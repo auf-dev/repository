919d35391da852595ab6d54af1cdcb46a4e33f39
= <=>
2024-12-21 16:41:18 +0800

    feat: Add some basic packages

---
+++ packages/brew.install.sh
@@ -0,0 +1,9 @@
+if [ <platform> == "macos" ]
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+elif [ <platform> == "linux" ]
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew
+fi
+
+
---
+++ packages/git.install.sh
@@ -0,0 +1,10 @@
+if [ <platform> == "windows" ]
+then
+  winget install --id Git.Git --version <version> -e --source winget
+elif [ <platform> == "macos" ]
+then
+  brew install git@<version>
+elif [ <platform> == "linux" ]
+then
+  brew install git@<version>
+fi
