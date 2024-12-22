142bfafb23f50e0e766ee3408ec1e0601289fb91
= <=>
2024-12-22 16:54:50 +0800

    fix: test sh

--- packages/git.install.sh
+++ packages/git.install.sh
@@ -1,10 +1,10 @@
-if [ <platform> == "windows" ]
+if [ "<platform>" == "windows" ]
 then
   winget install --id Git.Git --version <version> -e --source winget
-elif [ <platform> == "macos" ]
+elif [ "<platform>" == "macos" ]
 then
   brew install git@<version>
-elif [ <platform> == "linux" ]
+elif [ "<platform>" == "linux" ]
 then
   brew install git@<version>
 fi
